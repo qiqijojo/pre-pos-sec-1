326cb68593df307b01b52a01167e57ad2de3ac9c
wwwq77 <774948439@qq.com>
2016-05-27 21:01:58 +0800

    practice提交

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,12 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+	var arr = [];
+ 	for(var i=0;i<collection_a.length;i++){
+		var index = collection_b.indexOf(collection_a[i]);
+			if(index>=0){
+				arr.push(collection_a[i]);
+			}
+	}
+	return arr;
 }
 
 module.exports = collect_same_elements;
